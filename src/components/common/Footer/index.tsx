466b5af6d3b754369f8164ce3944370ba26c0e90
ThuLe <thuanhle219@gmail.com>
2024-05-13 07:59:46 +0700

    feat:Add footer component

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,20 +1,21 @@
 import { FOOTER_ITEM } from "@/constants/footer";
 import Link from "next/link";
 import React from "react";
-
+import Image from "next/image";
+import logo from '@/assets/logo.png'
 interface FooterProps {}
 
 export const Footer: React.FC<FooterProps> = () => {
     return (
-      <div className="container mx-auto flex flex-wrap items-center py-6 px-6">
+      <div className="container mx-auto flex flex-wrap items-center py-6 px-20">
         <div className="">
-          <h1>Travel Goo</h1>
+          <Image src={logo} alt ="" className = "max-w-14"></Image>
           <p>Book your trip in minute, get full Control for much longer. </p>
         </div>
         <div className="grow">
           <div className="flex items-center justify-center gap-20">
             {FOOTER_ITEM.map((item) => (
-              <div key={item.label} className="flex flex-col items-start">
+              <div key={item.label} className="flex flex-col items-start gap-y-8">
                 <Link href={item.href} className="text-black-secondary font-medium mb-2">
                   {item.label}
                 </Link>
@@ -22,7 +23,7 @@ export const Footer: React.FC<FooterProps> = () => {
                   <ul>
                     {item.subItem.map((subItem) => (
                       <li key={subItem.subLabel}>
-                        <Link href={subItem.href} className="text-gray-lighter font-normal">
+                        <Link href={subItem.href} className="text-gray-lighter font-normal hover:text-blue-light">
                           {subItem.subLabel}
                         </Link>
                       </li>
@@ -32,6 +33,9 @@ export const Footer: React.FC<FooterProps> = () => {
               </div>
             ))}
           </div>
+        </div>
+        <div className="flex-row">
+            <img src="" />
         </div>
       </div>
     );
